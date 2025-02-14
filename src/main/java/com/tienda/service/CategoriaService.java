5fdbd33a6405f71a652ad47a99518365a31a0e8b
FlavMora <franciscomh98@gmail.com>
2025-02-13 22:28:26 -0600

    Commit  4.1

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -1,14 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-
 package com.tienda.service;
 
-/**
- *
- * @author franm
- */
+import com.tienda.domain.Categoria; 
+import java.util.List;
+
 public interface CategoriaService {
 
+    //Se obtiene 
+    public Lis<Categoria> getCategorias(boolean activos);
 }
--- src/main/java/com/tienda/service/Impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/Impl/CategoriaServiceImpl.java
@@ -1,7 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import com.tienda.dao.CategoriaDao; 
+import com.tienda.domain.Categoria; 
+import com.tienda.service.CategoriaService; 
+import java.util.List; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service; 
+import org.springframework.transaction.annotation.Transactional;
 
 package com.tienda.service.Impl;
 
@@ -9,6 +12,19 @@ package com.tienda.service.Impl;
  *
  * @author franm
  */
-public class CategoriaServiceImpl {
+public class CategoriaServiceImpl implements  CategoriaService{
 
+
+@Autowired
+    private CategoriaDao categoriaDao;
+
+    @Override
+    @Transactional(readOnly=true)
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista=categoriaDao.findAll();
+        if (activos) {
+           lista.removeIf(e -> !e.isActivo());
+        }
+        return lista;
+    }
 }
